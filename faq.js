5b00ec055c7eb3e559b43b63224b625d84f4dfd5
DenisBardashevich <ackapbinka@mail.ru>
2025-03-20 16:58:45 +0300

    fix

--- faq.js
+++ faq.js
@@ -1,16 +1,20 @@
-let currentOpenDetails = null;
+document.addEventListener("DOMContentLoaded", () => {
+    let currentOpenDetails = null;
 
-function toggleDetails(id) {
-    const details = document.getElementById(id);
+    document.querySelectorAll(".faq-item").forEach((item, index) => {
+        item.addEventListener("click", () => {
+            const details = document.getElementById(`faq${index + 1}-details`);
 
-    if (currentOpenDetails === details) {
-        details.classList.toggle('show');
-        currentOpenDetails = null;
-    } else {
-        if (currentOpenDetails) {
-            currentOpenDetails.classList.remove('show');
-        }
-        details.classList.add('show');
-        currentOpenDetails = details;
-    }
-}
+            if (currentOpenDetails === details) {
+                details.classList.toggle("show");
+                currentOpenDetails = null;
+            } else {
+                if (currentOpenDetails) {
+                    currentOpenDetails.classList.remove("show");
+                }
+                details.classList.add("show");
+                currentOpenDetails = details;
+            }
+        });
+    });
+});
